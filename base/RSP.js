83639cdc991b486c36b13adba38431846eb151b7
JihoBae <jiho@dgu.ac.kr>
2021-06-29 16:23:11 +0900

    create game - RSP(Rock Scissors Paper)

---
+++ base/RSP.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+class RSP extends Component {
+  render() {
+    return (
+      <>
+        <h4>가위바위보 게임</h4>
+        <p></p>
+        <span></span>
+      </>
+    );
+  }
+}
+
+export default RSP;
--- base/client.js
+++ base/client.js
@@ -1,4 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import RSP from "./RSP";
 
-ReactDOM.render(</>, document.querySelector("#root"));
+ReactDOM.render(<RSP />, document.querySelector("#root"));

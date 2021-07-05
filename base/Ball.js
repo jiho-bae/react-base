02c37b9c684561da193058e094ab2c0a7ffa1e85
JihoBae <jiho@dgu.ac.kr>
2021-07-05 23:15:36 +0900

    change function component, useState, useEffect, useRef

--- base/Ball.js
+++ base/Ball.js
@@ -1,21 +1,18 @@
-import React, { PureComponent } from "react";
+import React from "react";
 
-class Ball extends PureComponent {
-  render() {
-    const { number } = this.props;
-    let background;
-    if (number <= 10) background = "#ff3838";
-    else if (number <= 20) background = "orange";
-    else if (number <= 30) background = "#fff200";
-    else if (number <= 40) background = "#0984e3";
-    else background = "green";
+const Ball = ({ number }) => {
+  let background;
+  if (number <= 10) background = "#ff3838";
+  else if (number <= 20) background = "orange";
+  else if (number <= 30) background = "#fff200";
+  else if (number <= 40) background = "#0984e3";
+  else background = "green";
 
-    return (
-      <div className="ball" style={{ background }}>
-        {number}
-      </div>
-    );
-  }
-}
+  return (
+    <div className="ball" style={{ background }}>
+      {number}
+    </div>
+  );
+};
 
 export default Ball;
--- base/Lotto.js
+++ base/Lotto.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import Ball from "./Ball";
 
 function getNumbers() {
@@ -13,78 +13,55 @@ function getNumbers() {
   return [...numbers, bonusNumber];
 }
 
-class Lotto extends Component {
-  state = {
-    numbers: getNumbers(),
-    balls: [],
-    bonus: null,
-    reDo: false,
-  };
-  timeouts = [];
+const Lotto = () => {
+  const [numbers, setNumbers] = useState(getNumbers());
+  const [balls, setBalls] = useState([]);
+  const [bonus, setBonus] = useState(null);
+  const [reDo, setReDo] = useState(false);
+  const timeouts = useRef([]);
 
-  getLottoNumbers = () => {
-    const { numbers } = this.state;
+  useEffect(() => {
+    console.log("useEffect");
     const len = numbers.length;
     for (let i = 0; i < len - 1; i++) {
-      this.timeouts[i] = setTimeout(() => {
-        this.setState((prevState) => {
-          return { balls: [...prevState.balls, numbers[i]] };
-        });
+      timeouts.current[i] = setTimeout(() => {
+        setBalls((prevBalls) => [...prevBalls, numbers[i]]);
       }, (i + 1) * 1000);
     }
-    this.timeouts[len - 1] = setTimeout(() => {
-      this.setState({
-        bonus: numbers[len - 1],
-        reDo: true,
-      });
+    timeouts.current[len - 1] = setTimeout(() => {
+      setBonus(numbers[len - 1]);
+      setReDo(true);
     }, len * 1000);
+    return () => {
+      timeouts.current.forEach((timeout) => clearTimeout(timeout));
+    };
+  }, [timeouts.current]);
+
+  const onClickReDo = () => {
+    setNumbers(getNumbers());
+    setBalls([]);
+    setBonus(null);
+    setReDo(false);
+    timeouts.current = [];
   };
 
-  componentDidMount() {
-    this.getLottoNumbers();
-  }
-
-  componentWillUnmount() {
-    this.timeouts.forEach((timeout) => clearTimeout(timeout));
-  }
-
-  componentDidUpdate(prevProps, prevState) {
-    console.log("update!");
-    if (!this.state.balls.length) {
-      this.getLottoNumbers();
-    }
-  }
-
-  onClickReDo = () => {
-    this.setState({
-      numbers: getNumbers(),
-      balls: [],
-      bonus: null,
-      reDo: false,
-    });
-    this.timeouts = [];
-  };
-  render() {
-    const { balls, bonus, reDo } = this.state;
-
-    return (
-      <>
-        <h4>로또 번호 게임</h4>
-        <div>당첨 숫자</div>
-        <div id="result">
-          {balls.map((v) => (
-            <Ball key={v} number={v} />
-          ))}
-        </div>
-        <br />
-        <div>보너스 숫자</div>
-        {bonus && <Ball number={bonus} />}
-        <br />
-        <br />
-        {reDo && <button onClick={this.onClickReDo}>한번 더</button>}
-      </>
-    );
-  }
-}
+  return (
+    <>
+      <h4>로또 번호 게임</h4>
+      <div>당첨 숫자</div>
+      <div id="result">
+        {balls.map((v) => (
+          <Ball key={v} number={v} />
+        ))}
+      </div>
+      <br />
+      <div>보너스 숫자</div>
+      {bonus && <Ball number={bonus} />}
+      <br />
+      <br />
+      {reDo && <button onClick={onClickReDo}>한번 더</button>}
+    </>
+  );
+};
 
 export default Lotto;

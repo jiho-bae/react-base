9bd18389a57e0a66905c3a8a06e4a60d4e585ca6
JihoBae <jiho@dgu.ac.kr>
2021-07-07 02:13:36 +0900

    use useEffect to judge win, draw and lose.

--- base/Td.js
+++ base/Td.js
@@ -1,12 +1,12 @@
 import React, { useCallback } from "react";
-import { CHANGE_TURN, CLICK_CELL } from "./TicTacToe";
+import { CLICK_CELL } from "./TicTacToe";
 
 const Td = ({ rowIdx, colIdx, cellData, dispatch }) => {
   const onClickTd = useCallback(() => {
     console.log(rowIdx, colIdx);
+    if (cellData) return;
     dispatch({ type: CLICK_CELL, row: rowIdx, col: colIdx });
-    dispatch({ type: CHANGE_TURN });
-  }, []);
+  }, [cellData]);
 
   return <td onClick={onClickTd}>{cellData}</td>;
 };
--- base/TicTacToe.js
+++ base/TicTacToe.js
@@ -1,4 +1,4 @@
-import React, { useCallback, useReducer } from "react";
+import React, { useEffect, useCallback, useReducer } from "react";
 import Table from "./Table";
 
 const initialState = {
@@ -9,11 +9,13 @@ const initialState = {
     ["", "", ""],
     ["", "", ""],
   ],
+  recentCell: [-1, -1],
 };
 
 export const SET_WINNER = "SET_WINNER";
 export const CLICK_CELL = "CLICK_CELL";
 export const CHANGE_TURN = "CHANGE_TURN";
+export const RESET_TABLE = "RESET_TABLE";
 
 const reducer = (state, action) => {
   switch (action.type) {
@@ -29,19 +31,61 @@ const reducer = (state, action) => {
       return {
         ...state,
         tableData,
+        recentCell: [action.row, action.col],
       };
     case CHANGE_TURN:
       return {
         ...state,
         turn: state.turn === "O" ? "X" : "O",
       };
+    case RESET_TABLE:
+      return {
+        ...state,
+        winner: action.winner === "" ? action.winner : state.winner,
+        turn: "O",
+        tableData: [
+          ["", "", ""],
+          ["", "", ""],
+          ["", "", ""],
+        ],
+        recentCell: [-1, -1],
+      };
+    default:
+      return state;
   }
 };
 const TicTacToe = () => {
   const [state, dispatch] = useReducer(reducer, initialState);
+  const { tableData, turn, recentCell } = state;
 
   const onClickTable = useCallback(() => {}, []);
 
+  useEffect(() => {
+    console.log("??");
+    const [row, col] = recentCell;
+    if (row < 0) return;
+    let win = false;
+    if (tableData[row][0] === turn && tableData[row][1] === turn && tableData[row][2] === turn) win = true;
+    if (tableData[0][col] === turn && tableData[1][col] === turn && tableData[2][col] === turn) win = true;
+    if (tableData[0][0] === turn && tableData[1][1] === turn && tableData[2][2] === turn) win = true;
+    if (tableData[2][0] === turn && tableData[1][1] === turn && tableData[0][2] === turn) win = true;
+    if (win) {
+      dispatch({ type: SET_WINNER, winner: turn });
+      dispatch({ type: RESET_TABLE });
+    } else {
+      let isFull = true;
+      tableData.forEach((row) => {
+        row.forEach((col) => {
+          if (!col) isFull = false;
+        });
+      });
+      if (isFull) {
+        dispatch({ type: RESET_TABLE, winner: "" });
+      } else {
+        dispatch({ type: CHANGE_TURN });
+      }
+    }
+  }, [recentCell]);
   return (
     <>
       <h4>틱택토(3x3 삼목게임)</h4>

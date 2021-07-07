8b1d98c0bf3d762b303cd0c85758e356a36a3297
JihoBae <jiho@dgu.ac.kr>
2021-07-07 13:33:24 +0900

    create game - MineSweeper

---
+++ base/MineSweeper.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Table from "./Table";
+
+const MineSweeper = () => {
+  return (
+    <>
+      <h4>지뢰찾기</h4>
+      <Table />
+    </>
+  );
+};
+
+export default MineSweeper;
---
+++ base/Td.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Td = () => {
+  return <td onClick={onClickTd}>{cellData}</td>;
+};
+
+export default Td;

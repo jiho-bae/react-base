5fff2d77ec6d036c6b6e1f9f33c05cd07828cd30
JihoBae <jiho@dgu.ac.kr>
2021-07-10 13:33:45 +0900

    Mine Sweeper

--- base/client.js
+++ base/client.js
@@ -1,5 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import MineSweeper from "./MineSweeper";
 
-ReactDOM.render(<MineSweeper />, document.querySelector("#root"));
+
+ReactDOM.render(, document.querySelector("#root"));
---
+++ 지뢰찾기/client.js
@@ -0,0 +1,5 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import MineSweeper from "./MineSweeper";
+
+ReactDOM.render(<MineSweeper />, document.querySelector("#root"));
